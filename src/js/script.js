417b40da37ec813f358f3060954e77446e70253e
seebasduqe <sedura96@gmail.com>
2024-11-08 18:45:51 -0500

    cambie nombre de columnas y host backend

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,25 @@
-fetch("https://fake-api-vq1l.onrender.com/posts", {
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEwLCJlbWFpbCI6InMuZHVxdWVAdXRwLmVkdS5jbyIsImlhdCI6MTcyNzQ3MDkwOSwiZXhwIjoxNzQ0NzUwOTA5fQ.LquNxNAn0Go149_lyKDbMc10QE2J60K_OQTLXkG_b3w"
-    }
+fetch("https://api-mountainbikeworld.onrender.com/api/bikes", {
+  method: "GET",
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEwLCJlbWFpbCI6InMuZHVxdWVAdXRwLmVkdS5jbyIsImlhdCI6MTcyNzQ3MDkwOSwiZXhwIjoxNzQ0NzUwOTA5fQ.LquNxNAn0Go149_lyKDbMc10QE2J60K_OQTLXkG_b3w"
+  }
 })
   .then(response => response.json())
   .then(data => {
 
     const container = document.getElementById('products-grid');
 
-    data.forEach( product => {
-      const images = JSON.parse(product.images);
+    data.forEach(product => {
+
+      console.log(product);
+
       const productDiv = `
             <div class="bg-gray-800 rounded-lg flex flex-col items-center justify-center w-full max-w-lg mx-auto"> 
-              <img class="w-55 h-64 mx-auto rounded-lg mb-4 shadow-lg shadow-teal-500" src="../public/${images[0] || 'default-image.png'}" alt="${product.title}">
+              <img class="w-55 h-64 mx-auto rounded-lg mb-4 shadow-lg shadow-teal-500" src="../public/${product.imageUrl || 'default-image.png'}" alt="${product.title}">
                 <div class="p-6">
                     <div class="flex items-center justify-between mb-2">
                         <h2 class="block text-xl font-semibold text-center text-white">${product.title}</h2>
-                        <p class="block text-gray-300 text-sm text-center">$${product.value.toLocaleString()}</p>
+                        <p class="block text-gray-300 text-sm text-center">$${product.price.toLocaleString()}</p>
                     </div>
                     <p class="block font-sans text-sm antialiased font-normal leading-normal text-gray-300 opacity-75 sm:text-xs md:text-sm">
                         ${product.description}
@@ -37,53 +39,61 @@ fetch("https://fake-api-vq1l.onrender.com/posts", {
                 </button>
             </div>
         `;
-        container.innerHTML += productDiv;
+      container.innerHTML += productDiv;
     })
+
+    productDiv_new = `<div class="bg-gray-800 rounded-lg flex flex-col items-center justify-center w-full max-w-lg mx-auto">
+          <div class="p-6">
+              <div class="flex items-center justify-between mb-2">
+                  <h2 class="block text-xl font-semibold text-center text-white">Añadir nueva bisicleta</h2>
+              </div>
+          </div>
+          <a href="registerBike.html" class="flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+              <span class="mx-1">+</span>
+          </a>
+      </div>`;
+
+      container.innerHTML += productDiv_new;
+
   });
 
 function sendForm() {
   const title = document.getElementById('title').value;
-    const value = document.getElementById('value').value;
-    const image = document.getElementById('image').value;
-    const description = document.getElementById('description').value;
-  
-    const body ={
-      title: title,
-      description: description,
-      value: value,
-      images: [image] 
-    }
-    
-    fetch("https://fake-api-vq1l.onrender.com/posts", {
-      method: "POST", 
-      headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEwLCJlbWFpbCI6InMuZHVxdWVAdXRwLmVkdS5jbyIsImlhdCI6MTcyNzQ3MDkwOSwiZXhwIjoxNzQ0NzUwOTA5fQ.LquNxNAn0Go149_lyKDbMc10QE2J60K_OQTLXkG_b3w",
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify(body)
-    })
-    .then( res => res.json())
-    .then( res => { 
-      console.log(
-        "respuesta de la api", res
-      )
+  const value = document.getElementById('value').value;
+  const image = document.getElementById('image').value;
+  const description = document.getElementById('description').value;
+
+  const body = {
+    title: title,
+    description: description,
+    price: value,
+    imageUrl: image
+  }
+
+  fetch("https://api-mountainbikeworld.onrender.com/api/bikes", {
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log("respuesta de la api", res);
+      window.location.reload();
     })
 }
 
 
-function deletePost(id){
+function deletePost(id) {
   console.log('entro');
-  fetch(`https://fake-api-vq1l.onrender.com/posts/${id}`, {
-    method: "DELETE", 
-    headers: {
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEwLCJlbWFpbCI6InMuZHVxdWVAdXRwLmVkdS5jbyIsImlhdCI6MTcyNzQ3MDkwOSwiZXhwIjoxNzQ0NzUwOTA5fQ.LquNxNAn0Go149_lyKDbMc10QE2J60K_OQTLXkG_b3w",
-    },
+  fetch(`https://api-mountainbikeworld.onrender.com/api/bikes/${id}`, {
+    method: "DELETE",
+    headers: {},
   })
-  .then( res => res.json())
-  .then( res => {
-    console.log(
-      "respuesta de la api", res
-    )
-    location.reload();
-  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(
+        "respuesta de la api", res
+      )
+      location.reload();
+    })
 }
